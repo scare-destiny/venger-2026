6d6f8a3024502d41f98669aef02ded537e75bc88
scare-destiny <zhenya.venger@gmail.com>
2025-09-22 14:29:53 +0200

    Change header element from h1 to h2 in About component for improved semantic structure.

--- src/components/React/About.tsx
+++ src/components/React/About.tsx
@@ -15,9 +15,9 @@ export default function About() {
       </div>
       <div className="lg:order-first lg:row-span-2">
         {/* <FadeIn> */}
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+        <h2 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
           {/* Welcome to my space! */}
-        </h1>
+        </h2>
         {/* </FadeIn> */}
         <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
           <p>
