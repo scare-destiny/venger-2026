bda2031f248bc88010015fad7315ac66fbd48586
scare-destiny <zhenya.venger@gmail.com>
2025-09-30 21:49:11 +0200

    Fix typo in Contact component header by changing "Contat Info" to "Contact Info" for accuracy.

--- src/components/React/Contact.tsx
+++ src/components/React/Contact.tsx
@@ -116,7 +116,7 @@ function ContactDetails() {
       {/* <Offices className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2" /> */}
 
       <Border className="mt-16 pt-16">
-        <h2 className="font-display text-base font-semibold text-neutral-950">Contat Info</h2>
+        <h2 className="font-display text-base font-semibold text-neutral-950">Contact Info</h2>
         <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
           <div>
             <dt className="font-semibold text-neutral-950">Name</dt>
